edffbb0230ccd57aabbcdaa17423a34f72705627
xvirs <rosales.xavier.eloy@gmail.com>
2021-07-13 12:46:42 -0300

    Este es el segundo commit que realizo despues de un commit erroneo

---
+++ app/src/main/java/com/xer/felizcumple/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.xer.felizcumple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Esta es una app que solo tiene una vista esta en el curso de google : Conseptos basicos de kotlin
+
+
+    }
+
+
+}
